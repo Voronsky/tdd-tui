376e9926ae1872a96f2024ed8722ae47df894d8b
Voronsky <roflocopter911@gmail.com>
2026-06-12 21:52:36 -0400

    final test changes

--- internal/uex/client_test.go
+++ internal/uex/client_test.go
@@ -6,9 +6,9 @@ import (
 	"github.com/tdd-tui/internal/uex"
 )
 
-func TestCommodityPricesAll(t *testing.T) {
-	dummyClient := uex.ClientConfig("https://api.uexcorp.space/2.0", "REDACTED")
-	resp, err := dummyClient.CommmodityPricesAll()
+func TestCommodities(t *testing.T) {
+	dummyClient := uex.ClientConfig("https://api.uexcorp.space/2.0", "")
+	resp, err := dummyClient.Commodities()
 	if err != nil {
 		t.Fatalf("Expected no error during unmarshal, got: %v", err)
 	}
@@ -21,5 +21,35 @@ func TestCommodityPricesAll(t *testing.T) {
 
 	t.Log("Test successful HTTP Code working")
 
-	t.Logf("Data returned: %v", resp.Data)
+	t.Logf("Commodities returned: %v, %d", resp.Data, len(resp.Data))
 }
+
+//TODO: Defunc
+//func TestCommodityPricesAll(t *testing.T) {
+//	dummyClient := uex.ClientConfig("https://api.uexcorp.space/2.0", "")
+//	resp, err := dummyClient.CommmodityPricesAll()
+//	if err != nil {
+//		t.Fatalf("Expected no error during unmarshal, got: %v", err)
+//	}
+//
+//	t.Log("Data retrieved")
+//
+//	if resp.HttpCode != 200 {
+//		t.Errorf("Expected HTTP 200, got %d", resp.HttpCode)
+//	}
+//
+//	t.Log("Test successful HTTP Code working")
+//
+//	t.Logf("Commodities returned: %v, %d", resp.Data, len(resp.Data))
+//}
+
+//TODO: Defunc
+//func TestCommodityAverages(t *testing.T) {
+//	dummyClient := uex.ClientConfig("https://api.uexcorp.space/2.0", "")
+//	avgs, err := dummyClient.CommoditiesAveragesAll()
+//	if err != nil {
+//		t.Fatalf("Expected no error during unmarshal, got: %v", err)
+//	}
+//	t.Log("Data retrieved")
+//	t.Logf("Commodities Averages returned: %v", avgs)
+//}
